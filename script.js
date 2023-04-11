607ee2e57b68759eb0f195fc207f6f94d07bb959
emilvn <emilvnielsen@hotmail.com>
2023-04-11 13:14:49 +0200

    it works

--- script.js
+++ script.js
@@ -2,5 +2,12 @@
 window.addEventListener("load", main);
 
 function main() {
-    
+    const arr = [];
+    let count = 0;
+    setInterval(() => {
+        count++;
+        arr.unshift(count);
+        arr.length = 10;
+        console.log(arr);
+    }, 1000);
 }
